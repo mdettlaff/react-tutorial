d4bb86818df93209fb4f25c4de714410811734ef
Michał Dettlaff <mdettlaff@jitsolutions.pl>
2020-01-06 16:42:42 +0100

    render board using loops

--- src/index.js
+++ src/index.js
@@ -23,23 +23,16 @@ class Board extends React.Component {
     }
 
     render() {
+        const boardSize = 3;
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {Array(boardSize).fill(null).map((v, row) => {
+                    return <div className="board-row">
+                        {Array(boardSize).fill(null).map((v, column) => {
+                            return this.renderSquare(row * boardSize + column);
+                        })}
+                    </div>
+                })}
             </div>
         );
     }
